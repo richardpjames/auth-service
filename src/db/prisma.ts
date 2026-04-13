f0761f6c037afb4d5ed74b97dd24078d6954c9b9
Richard James <richard@richardpjames.com>
2026-04-13 12:09:21 +0100

    Add ability to create a user

---
+++ src/db/prisma.ts
@@ -0,0 +1,8 @@
+// Set up prisma to connect to MariaDB (or MySQL)
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client.js';
+
+// Connect to the database and set up prisma
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+// Create the prisma client and allow export
+export const prisma = new PrismaClient({ adapter });
