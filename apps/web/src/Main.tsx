f1aaa14187775ef533b5b1ae022650375e84d791
Richard James <richard@richardpjames.com>
2026-04-15 10:20:45 +0100

    Improve chunking by adding lazy loading

--- apps/web/src/Main.tsx
+++ apps/web/src/Main.tsx
@@ -1,25 +1,34 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './Index.css';
-import NotFound from './NotFound.tsx';
-import Register from './Register.tsx';
-import LogInPage from './LogIn.tsx';
-import LoggedIn from './LoggedIn.tsx';
-import Logout from './Logout.tsx';
+
+const NotFound = lazy(() => import('./NotFound.tsx'));
+const Register = lazy(() => import('./Register.tsx'));
+const LogInPage = lazy(() => import('./LogIn.tsx'));
+const LoggedIn = lazy(() => import('./LoggedIn.tsx'));
+const Logout = lazy(() => import('./Logout.tsx'));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <div className="flex h-screen flex-col max-w-5xl mx-auto">
         <div className="w-full mt-5 px-5">
-          <Routes>
-            <Route path="/login" element={<LogInPage />} />
-            <Route path="/loggedin" element={<LoggedIn />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="w-full text-center">
+                <p className="loading"></p>
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/login" element={<LogInPage />} />
+              <Route path="/loggedin" element={<LoggedIn />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </div>
       </div>
     </BrowserRouter>
